574dbd1b4404f28cb27ddf41c63f76934fc9ef1e
Liam <will.leemans@gmail.com>
2019-12-10 21:50:46 +0100

    :sparkles: : day 3 - step 1

--- src/main/kotlin/fr/jetdev/three/CrossedWires.kt
+++ src/main/kotlin/fr/jetdev/three/CrossedWires.kt
@@ -11,7 +11,7 @@ fun computeWires(wire1: String, wire2: String): String {
     val intersect = listOf<Pair<Int, Int>>().toMutableList();
 
     wire1Points.stream().forEach { w1: Pair<Int, Int> ->
-        intersect.addAll(wire2Points.stream().filter {w1 == it }.collect(Collectors.toList()))
+        intersect.addAll(wire2Points.stream().filter { w1 == it }.collect(Collectors.toList()))
     }
 
     intersect.removeAt(0)
@@ -20,47 +20,46 @@ fun computeWires(wire1: String, wire2: String): String {
         .mapToInt { (x, y) -> x.absoluteValue + y.absoluteValue }
         .min()
 
-    return closestIntersection.orElse(-1 ).toString();
+    return closestIntersection.orElse(-1).toString();
 
 
 }
 
 private fun traceWire(wire: String): MutableList<Pair<Int, Int>> {
-    val wire1Points = listOf(Pair(0, 0)).toMutableList()
+    val wireCoord = listOf(Pair(0, 0)).toMutableList()
 
-    wire.split(",").stream()
-        .forEach {
-            val direction = it[0]
-            val distance = Integer.parseInt(it.substring(1));
+    wire.split(",").forEach {
+        val direction = it[0]
+        val distance = Integer.parseInt(it.substring(1));
 
-            when (direction) {
-                'R' -> {
-                    (0..distance).forEach { _ ->
-                        val (x, y) = wire1Points.last()
-                        wire1Points.add(Pair(x + 1, y))
-                    }
+        when (direction) {
+            'R' -> {
+                (0 until distance).forEach { _ ->
+                    val (x, y) = wireCoord.last()
+                    wireCoord.add(Pair(x + 1, y))
                 }
-                'D' -> {
-                    (0..distance).forEach { _ ->
-                        val (x, y) = wire1Points.last()
-                        wire1Points.add(Pair(x, y - 1))
-                    }
-
+            }
+            'D' -> {
+                (0 until distance).forEach { _ ->
+                    val (x, y) = wireCoord.last()
+                    wireCoord.add(Pair(x, y - 1))
                 }
-                'L' -> {
-                    (0..distance).forEach { _ ->
-                        val (x, y) = wire1Points.last()
-                        wire1Points.add(Pair(x - 1, y))
-                    }
 
+            }
+            'L' -> {
+                (0 until distance).forEach { _ ->
+                    val (x, y) = wireCoord.last()
+                    wireCoord.add(Pair(x - 1, y))
                 }
-                'U' -> {
-                    (0..distance).forEach { _ ->
-                        val (x, y) = wire1Points.last()
-                        wire1Points.add(Pair(x, y + 1))
-                    }
+
+            }
+            'U' -> {
+                (0 until distance).forEach { _ ->
+                    val (x, y) = wireCoord.last()
+                    wireCoord.add(Pair(x, y + 1))
                 }
             }
         }
-    return wire1Points
+    }
+    return wireCoord
 }
